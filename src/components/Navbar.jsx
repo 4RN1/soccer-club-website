4daac52842120d1c73f7351d9c89f677b3d6e4d6
Anri Begheladze <anriskr14@gmail.com>
2025-09-16 20:42:55 +0400

    added toggle-menu and mobile visual fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,34 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Link } from "react-router-dom";
 import '../css/Navbar.css'
 
 function Navbar() {
+  const [show, setShow] = useState(false)
 
-const [show , setShow] = useState(false)
+  return (
+    <div className='container'>
+    
 
-
-useEffect(()=> {
-  setShow(true)
-}, [])
-
-  return <div className={`navbar ${show ? "show" : ""}`}>
-    <ul >
-      <li><Link to="/">მთავარი</Link></li>
-        <li><Link to="/about">ჩვენს შესახებ</Link></li>
-        <li><Link to="/schedule">განრიგი / პროგრამები</Link></li>
-        <li><Link to="/news">ახალი ამბები</Link></li>
-        <li><Link to="/gallery">გალერეა</Link></li>
-        <li><Link to="/registration">რეგისტრაცია</Link></li>
-    </ul>
-  </div>
+      <div className={`navbar ${show ? "show" : ""}`}>
+           <Link to={'/'}><img src="/logo.png" alt="logo" className='logo' /></Link>
+        <ul className={`nav-links ${show ? 'active' : ""}`}>
+         
+          <li><Link to="/">მთავარი</Link></li>
+          <li><Link to="/about">ჩვენს შესახებ</Link></li>
+          <li><Link to="/schedule">განრიგი / პროგრამები</Link></li>
+          <li><Link to="/news">ახალი ამბები</Link></li>
+          <li><Link to="/gallery">გალერეა</Link></li>
+          <li><Link to="/registration">რეგისტრაცია</Link></li>
+        </ul>
+        <button
+          className='toggle-menu'
+          onClick={() => setShow(!show)}
+        >
+          {show ? '✖' : "☰"}
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
